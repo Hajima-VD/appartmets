5f31f230f97e08061e2f2024cec794d7f5e95ddd
Denis Voroniuc <122531721+Hajima-VD@users.noreply.github.com>
2023-10-27 15:02:01 +0300

    app2_27.10.23

--- js/main.js
+++ js/main.js
@@ -8,3 +8,15 @@ function sumElements() {
 }
 
 document.getElementById('result').innerHTML = sumElements();
+
+function sumElements2() {
+    let elements2 = document.querySelectorAll('#sum2');
+    let sum2 = 0;
+    for (let i = 0; i < elements2.length; i++) {
+        sum2 += parseInt(elements2[i].innerHTML);
+    }
+    return sum2;
+}
+
+document.getElementById('result2').innerHTML = sumElements2();
+
